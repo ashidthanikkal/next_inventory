11866be1dd15f5d57a4dfb2172ae48a350bbe82a
ashidthanikkal <ashidthanikkal@gmail.com>
2025-09-09 15:25:57 +0530

    --

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: "inventory-management",
+            name: "next_inventory",
             script: "npm",
             arg:"run dev",
             env: {
